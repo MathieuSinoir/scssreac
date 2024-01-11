2de81d8314e04411362eea4a1b55b275771d6594
mathi <sinoir.mathieu@gmail.com>
2024-01-11 16:10:21 +0100

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./style.scss";
+
+function App() {
+  return (
+    <div className="Card">
+      <h1>SASS Exemple</h1>
+      <h2>Comment fonctionne le SASS?</h2>
+      <p>
+        Pour installer et utiliser le préprocesseur CSS SASS sur ton projet
+        React, utilise la commande npm install sass. Après cela, il te suffit de
+        commencer à créer des fichiers .scss et de les importer dans React.
+        comme tu le ferais normalement avec du CSS. Cela fonctionne simplement
+      </p>
+      <p>Je prefere le CSS classique</p>
+    </div>
+  );
+}
+
+export default App;
